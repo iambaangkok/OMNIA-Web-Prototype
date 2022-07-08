0f0f910851b7fa18d10d34396c5f37798767ff39
Baangkok Vanijyananda <iambaangkok@gmail.com>
2022-07-08 08:45:23 +0700

    added lorem picsum fectching

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
 import './App.css';
 
 function App() {
+
+  const [itemPictureURL, setItemPictureURL] = useState<string>()
+
+  useEffect(() => {
+    fetchItem();
+  }, [])
+
+  const fetchItem = async () => {
+    const currentTimeSeconds = Date.now()/1000
+    const resp = await axios.get(`https://picsum.photos/seed/omnia${currentTimeSeconds}/1920/1080`, { responseType: "blob" })
+    //console.log(resp)
+    const url = URL.createObjectURL(resp.data)
+
+    setItemPictureURL(url)
+
+  }
   return (
     <div>
-      Yey
+      <div
+      className='w-full h-full color-white bg-black'
+      style={{
+        padding:"5%",
+        backgroundImage: `url(${itemPictureURL})`,
+      }}>
+      </div>
     </div>
   );
 }
