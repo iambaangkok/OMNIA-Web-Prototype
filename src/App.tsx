3e6a9afecbfeb0e838889a01b0df6caef8fab620
Baangkok Vanijyananda <iambaangkok@gmail.com>
2022-07-08 09:53:13 +0700

    added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 import './App.css';
+import LandingPage from './LandingPage';
 
 function App() {
 
-  const [itemPictureURL, setItemPictureURL] = useState<string>()
 
-  useEffect(() => {
-    fetchItem();
-  }, [])
 
-  const fetchItem = async () => {
-    const currentTimeSeconds = Date.now()/1000
-    const resp = await axios.get(`https://picsum.photos/seed/omnia${currentTimeSeconds}/1920/1080`, { responseType: "blob" })
-    //console.log(resp)
-    const url = URL.createObjectURL(resp.data)
-
-    setItemPictureURL(url)
-
-  }
-  return (
-    <div>
-      <div
-      className='w-full h-full color-white bg-black'
-      style={{
-        padding:"5%",
-        backgroundImage: `url(${itemPictureURL})`,
-      }}>
-      </div>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<LandingPage></LandingPage>}>
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/LandingPage.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+
+function LandingPage(){
+
+    const [itemPictureURL, setItemPictureURL] = useState<string>()
+
+    useEffect(() => {
+        fetchItem();
+    }, [])
+
+    const fetchItem = async () => {
+        const currentTimeSeconds = Date.now()/1000
+        const resp = await axios.get(`https://picsum.photos/seed/omnia${currentTimeSeconds}/1920/1080`, { responseType: "blob" })
+        //console.log(resp)
+        const url = URL.createObjectURL(resp.data)
+
+        setItemPictureURL(url)
+
+    }
+
+
+    return(
+        <div className='w-full'>
+            <div
+            className='w-full h-full color-white bg-black bg-size-fill'
+            style={{
+                padding:"0%",
+                backgroundImage: `url(${itemPictureURL})`,
+            }}>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
